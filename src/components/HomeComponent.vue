4f848add9e1b21e603dc9c648e8bca7932d6240a
Diego Valencia <ve18007@ues.edu.sv>
2022-10-13 15:54:43 -0600

    Agregando bootstrap

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,16 @@
+<!-- eslint-disable prettier/prettier -->
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  },
+};
+</script>
+
+<style></style>
